554484f715b67daac6faffc2e4de83058f8e33b1
TuTuKLK <geant.vert42@gmail.com>
2021-11-04 19:34:26 +0100

    maj dbconfig

--- dbConfig.js
+++ dbConfig.js
@@ -1,6 +1,13 @@
 var dbConfig = {
     server: 'localhost',
     // port: 1433,
+    // Authentication: {
+    //     type: 'default',
+    //     option: {
+    //         userName:'',
+    //         password:test
+    //     }
+    // },
     database: 'BuilderHeroes',
     driver: "msnodesqlv8",
     options: {
